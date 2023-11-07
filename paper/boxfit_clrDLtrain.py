94dc3e8999b7a28bbbf67af111edae5a29e68ce2
Oliver Boersma <omboersma@gmail.com>
2023-11-07 17:42:21 +0100

    Refactored training code [skip ci]

--- paper/boxfit_clrDLtrain.py
+++ paper/boxfit_clrDLtrain.py
@@ -1,105 +1,173 @@
-from CLR.clr_callback import CyclicLR
-from sklearn.preprocessing import StandardScaler
-import numpy as np
-from tensorflow.keras.losses import MeanAbsoluteError
-from keras import layers
-import keras
-from tensorflow.keras import backend as K
-import tensorflow as tf
-import pandas as pd
 import os
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras import layers, losses, optimizers, Sequential
+from tensorflow.keras.losses import MeanAbsoluteError
+from sklearn.preprocessing import StandardScaler
+from CLR.clr_callback import CyclicLR
+
+# Set CUDA device
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
+# Load datasets
+train_features = pd.read_csv("boxfitdata/boxfit_ism_final_trainfeatures.csv")
+test_features = pd.read_csv("boxfitdata/boxfit_ism_final_testfeatures.csv")
+train_labels = pd.read_csv("boxfitdata/boxfit_ism_final_trainlabels.csv")
+test_labels = pd.read_csv("boxfitdata/boxfit_ism_final_testlabels.csv")
 
-train_features = pd.read_csv(
-    'boxfitdata/boxfit_ism_final_trainfeatures.csv')
-test_features = pd.read_csv(
-    'boxfitdata/boxfit_ism_final_testfeatures.csv')
-train_labels = pd.read_csv(
-    'boxfitdata/boxfit_ism_final_trainlabels.csv')
-test_labels = pd.read_csv(
-    'boxfitdata/boxfit_ism_final_testlabels.csv')
-
-
+# Initialize and fit scalers
 scaler_in = StandardScaler()
 scaler_out = StandardScaler()
-
 train_features_scaled = scaler_in.fit_transform(train_features)
 train_labels_scaled = scaler_out.fit_transform(train_labels)
-
 test_features_scaled = scaler_in.transform(test_features)
 test_labels_scaled = scaler_out.transform(test_labels)
 
-
-filepath = 'boxfitfinal/'
+# Filepath for saving models
+filepath = "boxfitfinal/"
 
 
 def masked_metric(y_true, y_pred):
+    """Calculate the masked mean absolute error metric.
+
+    Args:
+        y_true: True labels.
+        y_pred: Predicted labels.
+
+    Returns:
+        The masked mean absolute error.
+    """
     mae = MeanAbsoluteError()
-    y_true_no_nan = tf.where(tf.math.is_nan(
-        y_true), tf.zeros_like(y_true), y_true)
-    y_pred_no_nan = tf.where(tf.math.is_nan(
-        y_true), tf.zeros_like(y_pred), y_pred)
-    non_zero_cor = tf.cast(tf.size(y_true_no_nan, out_type=tf.int32), dtype=tf.float32) / \
-        tf.math.count_nonzero(y_true_no_nan, dtype=tf.float32)
-    return mae(y_true_no_nan, y_pred_no_nan)*non_zero_cor
+    mask = ~tf.math.is_nan(y_true)
+    y_true_no_nan = tf.where(mask, y_true, tf.zeros_like(y_true))
+    y_pred_no_nan = tf.where(mask, y_pred, tf.zeros_like(y_pred))
+    non_zero_count = tf.reduce_sum(tf.cast(mask, tf.float32))
+    return mae(y_true_no_nan, y_pred_no_nan) * (
+        tf.size(y_true, out_type=tf.float32) / non_zero_count
+    )
 
 
-class CustomAccuracy(tf.keras.losses.Loss):
-    def __init__(self):
-        super().__init__()
+class CustomAccuracy(losses.Loss):
+    """Custom accuracy class that calculates the masked mean absolute error."""
 
     def call(self, y_true, y_pred):
-        mae = MeanAbsoluteError()
-        y_true_no_nan = tf.where(tf.math.is_nan(
-            y_true), tf.zeros_like(y_true), y_true)
-        y_pred_no_nan = tf.where(tf.math.is_nan(
-            y_true), tf.zeros_like(y_pred), y_pred)
-        non_zero_cor = tf.cast(tf.size(y_true_no_nan, out_type=tf.int32), dtype=tf.float32) / \
-            tf.math.count_nonzero(y_true_no_nan, dtype=tf.float32)
-        return mae(y_true_no_nan, y_pred_no_nan)*non_zero_cor
+        return masked_metric(y_true, y_pred)
 
 
-def build_and_compile_model():
-    model = keras.Sequential([
-        layers.Dense(
-            1000, input_dim=train_features_scaled.shape[1], activation='softplus'),
-        layers.Dense(1000, activation='softplus'),
-        layers.Dense(1000, activation='softplus'),
-        layers.Dense(117, activation='linear')
-    ])
+def build_and_compile_model(input_shape):
+    """Build and compile the neural network model.
 
-    model.compile(loss=CustomAccuracy(), metrics=[masked_metric],
-                  optimizer=keras.optimizers.Nadam(0.001))
+    Args:
+        input_shape: Shape of the input data.
+
+    Returns:
+        A compiled Keras model.
+    """
+    model = Sequential(
+        [
+            layers.Dense(1000, input_shape=input_shape, activation="softplus"),
+            layers.Dense(1000, activation="softplus"),
+            layers.Dense(1000, activation="softplus"),
+            layers.Dense(117, activation="linear"),
+        ]
+    )
+
+    model.compile(
+        loss=CustomAccuracy(),
+        metrics=[masked_metric],
+        optimizer=optimizers.Nadam(0.001),
+    )
     return model
 
 
-trainingsizes = [int(len(train_features_scaled)/32), int(len(train_features_scaled)/16), int(
-    len(train_features_scaled)/8), int(len(train_features_scaled)/4), int(len(train_features_scaled)/2)]
+def train_model(
+    model,
+    features,
+    labels,
+    batch_size,
+    clr_step_size,
+    base_lr,
+    max_lr,
+    mode,
+    epochs,
+    filepath,
+    Ntrain,
+):
+    """Train the model with the given dataset and hyperparameters.
+
+    Args:
+        model: The neural network model to train.
+        features: Input features for training.
+        labels: True labels for training.
+        batch_size: Size of the batches for training.
+        clr_step_size: Step size for cyclic learning rate.
+        base_lr: Base learning rate.
+        max_lr: Maximum learning rate.
+        mode: Mode for cyclic learning rate.
+        epochs: Number of epochs to train.
+        filepath: Path to save the trained model.
+        Ntrain: Number of training samples.
+    """
+    clr = CyclicLR(base_lr=base_lr, max_lr=max_lr, step_size=clr_step_size, mode=mode)
+    history = model.fit(
+        features,
+        labels,
+        validation_split=0.0,
+        batch_size=batch_size,
+        verbose=1,
+        epochs=epochs,
+        callbacks=[clr],
+    )
+    model.save(filepath + "boxfit_ism_stdsc_" + str(Ntrain) + ".h5")
+
+
+def evaluate_model(model, features, labels, scaler):
+    """Evaluate the model on the test dataset and print error metrics.
+
+    Args:
+        model: The trained neural network model.
+        features: Input features for evaluation.
+        labels: True labels for evaluation.
+        scaler: Scaler used to inverse transform the predictions.
+    """
+    test_predictions_scaled = model.predict(features)
+    test_predictions_unscaled = scaler.inverse_transform(test_predictions_scaled)
+    test_predictions = 10**test_predictions_unscaled
+    test_labels_lin = 10**labels
+    err = np.abs(test_predictions - test_labels_lin) / test_labels_lin
+    err = err.values.flatten()
+    print("errors <0.1: " + str(np.mean(err < 0.1)))
+    print("errors >0.2: " + str(np.mean(err > 0.2)))
+    print("median errors: " + str(np.nanmedian(err)))
+
+
+# Training loop with different training sizes
+trainingsizes = [len(train_features_scaled) // 2**i for i in range(6)]
 for Ntrain in trainingsizes:
-    dnn_model = build_and_compile_model()
+    dnn_model = build_and_compile_model((train_features_scaled.shape[1],))
     dnn_model.summary()
-    train_features_scaled_subset = train_features_scaled[0:Ntrain]
-    train_labels_scaled_subset = train_labels_scaled[0:Ntrain]
+    train_features_scaled_subset = train_features_scaled[:Ntrain]
+    train_labels_scaled_subset = train_labels_scaled[:Ntrain]
     batch_size = 128
-    clr_step_size = int(4 * (len(train_features_scaled_subset)/batch_size))
+    clr_step_size = int(4 * (len(train_features_scaled_subset) / batch_size))
     base_lr = 1e-4
     max_lr = 1e-2
-    mode = 'triangular2'
-    clr = CyclicLR(base_lr=base_lr, max_lr=max_lr,
-                   step_size=clr_step_size, mode=mode)
-    history = dnn_model.fit(train_features_scaled_subset, train_labels_scaled_subset,
-                            validation_split=0.0, batch_size=batch_size, verbose=1, epochs=200, callbacks=[clr])
+    mode = "triangular2"
+    epochs = 200
 
-    dnn_model.save(filepath+'boxfit_ism_stdsc_'+str(Ntrain)+'.h5')
+    train_model(
+        dnn_model,
+        train_features_scaled_subset,
+        train_labels_scaled_subset,
+        batch_size,
+        clr_step_size,
+        base_lr,
+        max_lr,
+        mode,
+        epochs,
+        filepath,
+        Ntrain,
+    )
 
-    test_predictions_scaled = dnn_model.predict(test_features_scaled)
-    test_predictions_unscaled = scaler_out.inverse_transform(
-        test_predictions_scaled)
-    test_predictions = 10**test_predictions_unscaled
-    test_labels_lin = 10**test_labels
-    err = np.abs(test_predictions-test_labels_lin)/test_labels_lin
-    err = err.values.flatten()
-    print('errors <0.1: '+str(len(err[err < 0.1])/len(err)))
-    print('errors >0.2: '+str(len(err[err > 0.2])/len(err)))
-    print('median errors: '+str(np.nanmedian(err)))
+    evaluate_model(dnn_model, test_features_scaled, test_labels, scaler_out)
--- paper/boxfit_clrDLtrain_wind.py
+++ paper/boxfit_clrDLtrain_wind.py
@@ -1,105 +1,108 @@
-from CLR.clr_callback import CyclicLR
-from sklearn.preprocessing import StandardScaler
-import numpy as np
-from tensorflow.keras.losses import MeanAbsoluteError
-from keras import layers
-import keras
-from tensorflow.keras import backend as K
-import tensorflow as tf
-import pandas as pd
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras import layers, losses, optimizers, Sequential, callbacks
+from tensorflow.keras.losses import MeanAbsoluteError
+from sklearn.preprocessing import StandardScaler
+from CLR.clr_callback import CyclicLR
+import matplotlib
 
+# Set CUDA device and matplotlib backend
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 matplotlib.use('Agg')
 
-train_features = pd.read_csv(
-    'boxfitdata/boxfit_wind_final_trainfeatures.csv')
-test_features = pd.read_csv(
-    'boxfitdata/boxfit_wind_final_testfeatures.csv')
-train_labels = pd.read_csv(
-    'boxfitdata/boxfit_wind_final_trainlabels.csv')
-test_labels = pd.read_csv(
-    'boxfitdata/boxfit_wind_final_testlabels.csv')
+# Load datasets
+train_features = pd.read_csv('boxfitdata/boxfit_wind_final_trainfeatures.csv')
+test_features = pd.read_csv('boxfitdata/boxfit_wind_final_testfeatures.csv')
+train_labels = pd.read_csv('boxfitdata/boxfit_wind_final_trainlabels.csv')
+test_labels = pd.read_csv('boxfitdata/boxfit_wind_final_testlabels.csv')
 
+# Initialize and fit scalers
 scaler_in = StandardScaler()
 scaler_out = StandardScaler()
-
 train_features_scaled = scaler_in.fit_transform(train_features)
-test_features_scaled = scaler_in.transform(test_features)
-
-
 train_labels_scaled = scaler_out.fit_transform(train_labels)
+test_features_scaled = scaler_in.transform(test_features)
 test_labels_scaled = scaler_out.transform(test_labels)
 
-batch_size = 128
-clr_step_size = int(4 * (len(train_features_scaled)/batch_size))
-base_lr = 1e-4
-max_lr = 1e-2
-mode = 'triangular2'
-clr = CyclicLR(base_lr=base_lr, max_lr=max_lr,
-               step_size=clr_step_size, mode=mode)
+# Filepath for saving models
 filepath = 'boxfitfinal/'
 filepath_intermediate = 'boxfitfinal/model-wind-stdsc-{epoch:02d}.hdf5'
-model_checkpoint_callback = keras.callbacks.ModelCheckpoint(
-    filepath_intermediate, monitor='loss', verbose=0, save_best_only=False, save_weights_only=False, mode='auto', period=20)
-
 
 def masked_metric(y_true, y_pred):
+    """Calculate the masked mean absolute error metric.
+    
+    Args:
+        y_true: True labels.
+        y_pred: Predicted labels.
+    
+    Returns:
+        The masked mean absolute error.
+    """
     mae = MeanAbsoluteError()
-    y_true_no_nan = tf.where(tf.math.is_nan(
-        y_true), tf.zeros_like(y_true), y_true)
-    y_pred_no_nan = tf.where(tf.math.is_nan(
-        y_true), tf.zeros_like(y_pred), y_pred)
-    non_zero_cor = tf.cast(tf.size(y_true_no_nan, out_type=tf.int32), dtype=tf.float32) / \
-        tf.math.count_nonzero(y_true_no_nan, dtype=tf.float32)
-    return mae(y_true_no_nan, y_pred_no_nan)*non_zero_cor
+    mask = ~tf.math.is_nan(y_true)
+    y_true_no_nan = tf.where(mask, y_true, tf.zeros_like(y_true))
+    y_pred_no_nan = tf.where(mask, y_pred, tf.zeros_like(y_pred))
+    non_zero_count = tf.reduce_sum(tf.cast(mask, tf.float32))
+    return mae(y_true_no_nan, y_pred_no_nan) * (tf.size(y_true, out_type=tf.float32) / non_zero_count)
 
-
-class CustomAccuracy(tf.keras.losses.Loss):
-    def __init__(self):
-        super().__init__()
-
+class CustomAccuracy(losses.Loss):
+    """Custom accuracy class that calculates the masked mean absolute error."""
     def call(self, y_true, y_pred):
-        mae = MeanAbsoluteError()
-        y_true_no_nan = tf.where(tf.math.is_nan(
-            y_true), tf.zeros_like(y_true), y_true)
-        y_pred_no_nan = tf.where(tf.math.is_nan(
-            y_true), tf.zeros_like(y_pred), y_pred)
-        non_zero_cor = tf.cast(tf.size(y_true_no_nan, out_type=tf.int32), dtype=tf.float32) / \
-            tf.math.count_nonzero(y_true_no_nan, dtype=tf.float32)
-        return mae(y_true_no_nan, y_pred_no_nan)*non_zero_cor
+        return masked_metric(y_true, y_pred)
 
-
-def build_and_compile_model():
-    model = keras.Sequential([
-        layers.Dense(
-            1000, input_dim=train_features_scaled.shape[1], activation='softplus'),
+def build_and_compile_model(input_shape):
+    """Build and compile the neural network model.
+    
+    Args:
+        input_shape: Shape of the input data.
+    
+    Returns:
+        A compiled Keras model.
+    """
+    model = Sequential([
+        layers.Dense(1000, input_shape=input_shape, activation='softplus'),
         layers.Dense(1000, activation='softplus'),
         layers.Dense(1000, activation='softplus'),
         layers.Dense(117, activation='linear')
     ])
 
     model.compile(loss=CustomAccuracy(), metrics=[masked_metric],
-                  optimizer=keras.optimizers.Nadam(0.001))
+                  optimizer=optimizers.Nadam(0.001))
     return model
 
-
-dnn_model = build_and_compile_model()
+# Build and compile the model
+dnn_model = build_and_compile_model((train_features_scaled.shape[1],))
 dnn_model.summary()
 
+# Set up training parameters
+batch_size = 128
+clr_step_size = int(4 * (len(train_features_scaled) / batch_size))
+base_lr = 1e-4
+max_lr = 1e-2
+mode = 'triangular2'
+
+# Set up callbacks
+clr = CyclicLR(base_lr=base_lr, max_lr=max_lr, step_size=clr_step_size, mode=mode)
+model_checkpoint_callback = callbacks.ModelCheckpoint(
+    filepath_intermediate, monitor='loss', verbose=0, save_best_only=False, save_weights_only=False, mode='auto', period=20)
+
+# Train the model
 history = dnn_model.fit(train_features_scaled, train_labels_scaled,
                         validation_split=0.0, batch_size=batch_size, verbose=1, epochs=2000, callbacks=[clr, model_checkpoint_callback])
 
-dnn_model.save(filepath+'boxfit_wind_final_stdsc.h5')
+# Save the final model
+dnn_model.save(filepath + 'boxfit_wind_final_stdsc.h5')
 
-
+# Evaluate the model
 test_predictions_scaled = dnn_model.predict(test_features_scaled)
-test_predictions_unscaled = scaler_out.inverse_transform(
-    test_predictions_scaled)
-test_predictions = 10**test_predictions_unscaled
-test_labels = 10**test_labels
-err = np.abs(test_predictions-test_labels)/test_predictions
+test_predictions_unscaled = scaler_out.inverse_transform(test_predictions_scaled)
+test_predictions = 10 ** test_predictions_unscaled
+test_labels = 10 ** test_labels
+err = np.abs(test_predictions - test_labels) / test_labels
 err = err.values.flatten()
-print('errors <0.1: '+str(len(err[err < 0.1])/len(err)))
-print('errors >0.2: '+str(len(err[err > 0.2])/len(err)))
-print('median errors: '+str(np.nanmedian(err)))
+print('errors <0.1: ' + str(np.mean(err < 0.1)))
+print('errors >0.2: ' + str(np.mean(err > 0.2)))
+print('median errors: ' + str(np.nanmedian(err)))
+
